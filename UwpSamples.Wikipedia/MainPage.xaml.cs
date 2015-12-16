885a0ebad8ff29f1c7ac0e901b37258df1ac620e
Roman Novitskiy <roman.novitsky@outlook.com>
2015-12-16 21:34:11 +0100

    Initial commit

---
+++ UwpSamples.Wikipedia/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace UwpSamples.Wikipedia
+{
+    /// <summary>
+    ///     An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        #region Constructors and Destructors
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
